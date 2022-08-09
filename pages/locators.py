68bc15ddda1ddb41bc82499067796b79bf617e4a
Michael Gavrilenko <gavr.inbox@gmail.com>
2022-08-09 14:34:30 +0200

    Added four locators for product page

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.ID, 'login_form')
     LOGIN_REGISTER_FORM = (By.ID, 'register_forma')
+    
+class ProductPageLocators():
+    BUTTON_ADD_TO_CART = (By.CLASS_NAME, 'btn-add-to-basket')
+    PRISE_PRODUCT = (By.CSS_SELECTOR, '.product_main > p')
+    NAME_PRODUCT = (By.CSS_SELECTOR, '.product_main > h1')
+    IN_BASKET_PRISE = (By.CSS_SELECTOR, '#messages > div:nth-child(3) > .alertinner > p > strong')
+    IN_BASKET_PRODUCT = (By.CSS_SELECTOR, '#messages > div:nth-child(1) > .alertinner > strong')
