01cd27c8aa602fb8169118f9f70920f9e427daa5
Michael Gavrilenko <gavr.inbox@gmail.com>
2022-08-09 17:01:15 +0200

    Added fikstura for find bug in differenet similar URLs

--- test_product_page.py
+++ test_product_page.py
@@ -1,12 +1,11 @@
 import pytest
 from .pages.product_page import ProductPage
 
-# link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-
-def test_guest_can_add_product_to_basket(browser):
-    page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # вызываем метод из BasePage<-MainPage - открываем страницу
-    page.click_button_add_to_cart()  # выполняем метод страницы — жмем кнопку
+@pytest.mark.parametrize('post', [*range(0, 7), pytest.param(7, marks=pytest.mark.xfail(reason='some bug')), *range(8, 10)])
+def test_guest_can_add_product_to_basket(browser, post):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{post}'
+    page = ProductPage(browser, link)
+    page.open()                      
+    page.click_button_add_to_cart()
     page.solve_quiz_and_get_code()
     page.should_be_added_to_cart()
