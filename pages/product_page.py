e2ae7340005e34f4d626069f83288b7e1d89f7e2
Michael Gavrilenko <gavr.inbox@gmail.com>
2022-08-09 14:33:10 +0200

    Created two method for product page

---
+++ pages/product_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+
+   def click_button_add_to_cart(self):
+      self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_CART).click()
+
+   def should_be_added_to_cart(self):
+      self.is_product_in_cart_equal()
+      self.is_prise_in_cart_equal()
+
+   def is_product_in_cart_equal(self):
+      product_in_description = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT).text
+      product_in_cart = self.browser.find_element(*ProductPageLocators.IN_BASKET_PRODUCT).text
+      assert product_in_description == product_in_cart, f"Product in the cart ({product_in_cart}) is not equal to choosed product ({product_in_description})"
+
+   def is_prise_in_cart_equal(self):
+      prise_in_description = self.browser.find_element(*ProductPageLocators.PRISE_PRODUCT).text
+      prise_in_cart = self.browser.find_element(*ProductPageLocators.IN_BASKET_PRISE).text
+      assert prise_in_description == prise_in_cart, f"Product in the cart ({prise_in_cart}) is not equal to choosed product ({prise_in_description})"
