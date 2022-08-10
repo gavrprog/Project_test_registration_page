7e8f8c80b0007d5fb7fde4676c08869796260a24
Michael Gavrilenko <gavr.inbox@gmail.com>
2022-08-10 15:21:16 +0200

    Deleted class and placed the cap

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,13 +1,9 @@
 from .base_page import BasePage
-from .locators import MainPageLocators
 
 class MainPage(BasePage):
-
-   def go_to_login_page(self):
-      login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-      login_link.click()
-
-   def should_be_login_link(self):
-      assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
-#       self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
-  
+    '''
+     - Заглушка - 
+    Конструктор с ключевым словом super на самом деле только вызывает конструктор класса предка и передает ему все те аргументы, которые мы передали в конструктор MainPage. 
+    '''
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
