23583f2915ea29e6087829fa3e01d5eb22b9097f
Michael Gavrilenko <gavr.inbox@gmail.com>
2022-08-10 15:19:36 +0200

    Added locators for BasketPage, deletd for MainPage

--- pages/locators.py
+++ pages/locators.py
@@ -1,7 +1,9 @@
 from selenium.webdriver.common.by import By
 
-class MainPageLocators():
+class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    MINI_BASKET = (By.CSS_SELECTOR, ".basket-mini .btn-group > a")
 
 class LoginPageLocators():
     LOGIN_FORM = (By.ID, 'login_form')
@@ -13,3 +15,8 @@ class ProductPageLocators():
     NAME_PRODUCT = (By.CSS_SELECTOR, '.product_main > h1')
     IN_BASKET_PRISE = (By.CSS_SELECTOR, '#messages > div:nth-child(3) > .alertinner > p > strong')
     IN_BASKET_PRODUCT = (By.CSS_SELECTOR, '#messages > div:nth-child(1) > .alertinner > strong')
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, '#messages > div:nth-child(1)')
+
+class BasketPageLocators():
+    BASKET_EMPTY = (By.CSS_SELECTOR, '#content_inner > p')
+    BASKET_FULL = (By.CSS_SELECTOR, '#content_inner > h2')
