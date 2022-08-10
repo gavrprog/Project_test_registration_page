e80a5f50041d51050b9ba9ca1332a5687916ef33
Michael Gavrilenko <gavr.inbox@gmail.com>
2022-08-10 18:14:37 +0200

    Two methods moved to common class

--- test_main_page.py
+++ test_main_page.py
@@ -10,20 +10,24 @@ def go_to_login_page(browser):
    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
    login_link.click()
 
-@pytest.mark.skip
-def test_guest_can_go_to_login_page(browser):
-   page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-   page.open()                      # вызываем метод из BasePage<-MainPage - открываем страницу
-   page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-#   page.should_be_login_link()
-   login_page = LoginPage(browser, browser.current_url)
-   login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+
+   def test_guest_can_go_to_login_page(self, browser):
+      link = "http://selenium1py.pythonanywhere.com"
+      page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+      page.open()                      # вызываем метод из BasePage<-MainPage - открываем страницу
+      page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+      login_page = LoginPage(browser, browser.current_url)
+      login_page.should_be_login_page()
+   
+   def test_guest_should_see_login_link(self, browser):
+      link = "http://selenium1py.pythonanywhere.com/"
+      page = MainPage(browser, link)
+      page.open()
+      page.should_be_login_link()
 
 @pytest.mark.skip
-def test_guest_can_go_to_login_page(browser):
-   browser.get(link)
-   go_to_login_page(browser)
-
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
    link = 'http://selenium1py.pythonanywhere.com/ru/'
    page = BasketPage(browser, link)
